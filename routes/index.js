f5a7bffae2ea31f840363546a894c6d164472e10
JapSir <japsircode@gmail.com>
2023-01-22 17:24:36 +0100

    add forms and display input

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const messages= [{
+  text: "hi there!",
+  user: "Amando",
+  added: new Date()
+}, {
+  text: "Hello World!",
+  user: "Charles",
+  added: new Date()
+}]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages= [{
-    text: "hi there!",
-    user: "Amando",
-    added: new Date()
-  }, {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }]
-
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+router.get("/new", function(req, res, next) {
+  res.render("form", {title: "Form Input"});
+})
+router.post("/new", (req, res) => {
+  messages.push({text: req.body.text, user: req.body.name, added: new Date()});
+  res.redirect("/")
+})
+
 
 module.exports = router;
